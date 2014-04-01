7a5e3cd9e12c7f6b3ef385406f06f1c1a871ce3c
Stuart Nelson <stuartnelson3@gmail.com>
2014-04-01 19:38:16 -0400

    Check list of known filters; add more flexible regex parser for interpolation

--- app/assets/javascripts/angular/services/variable_interpolator.js
+++ app/assets/javascripts/angular/services/variable_interpolator.js
@@ -2,8 +2,15 @@ angular.module("Prometheus.services").factory('VariableInterpolator', function()
   var re = /{{.+}}/g;
   var re1 = /{{\s?(\w+)\s?}}/g;
   var re2 = /{{\s?\w+\s?\|\s?\w+\s?}}/g;
+  var re3 = new RegExp("{{\\s?\\w+\\s?\\|\\s?\\w+(:('|\")?[a-zA-Z(?=:!^)]+('|\")?){0,}\\s?}}", "g");
 
   return function(str, varValues, scope) {
+    function knownFilters() {
+      return angular.module("Prometheus.filters")
+        ._invokeQueue
+        .map(function(q) { return q[2][0]; });
+    }
+
     var vars = str.match(re);
     if (!vars) {
       return str;
@@ -12,7 +19,7 @@ angular.module("Prometheus.services").factory('VariableInterpolator', function()
 
     // Deal with filtered variables.
     var pipedVars = [];
-    var filteredMatches = vars.match(re2)
+    var filteredMatches = vars.match(re3)
     if (filteredMatches) {
       pipedVars = pipedVars.concat(filteredMatches)
     }
@@ -22,14 +29,18 @@ angular.module("Prometheus.services").factory('VariableInterpolator', function()
       pipedVars.forEach(function(v) { pipeObj[v] = null; });
       var newStr = str
       pipedVars.forEach(function(match) {
-        var rep = match.replace(/\s+/g, '').replace(/{|}/g, '').match(/\s?(\w+)|(\w+)\s?/g)
+        var rep = match.replace(/\s+/g, '').replace(/{|}/g, '').split("|")
+        // .match(/\s?(\w+)|(\w+)\s?/g)
 
         // set on scope so we can $eval
         scope[rep[0]] = varValues[rep[0]]
-        var out = scope.$eval(rep.join("|"))
-        pipeObj[match] = out
-        // cleanup...
-        scope[rep[0]] = undefined
+        // check to see if rep[1] is in list of function
+        if (knownFilters().indexOf(rep[1].split(":")[0]) > -1) {
+          var result = scope.$eval(rep.join("|"))
+          pipeObj[match] = result
+          // cleanup...
+          scope[rep[0]] = undefined
+        }
       });
     }
 
